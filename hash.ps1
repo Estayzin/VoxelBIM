a5317dd40ffa97e25abec4fea01e01639258f467
Estayzin <35824466+Estayzin@users.noreply.github.com>
2026-03-19 16:18:18 -0300

    Actualización

--- hash.ps1
+++
@@ -1,6 +0,0 @@
-$text = 'admin:12345678'
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($text)
-$sha256 = [System.Security.Cryptography.SHA256]::Create()
-$hash = $sha256.ComputeHash($bytes)
-$hex = ($hash | ForEach-Object { $_.ToString('x2') }) -join ''
-Write-Host $hex
--- push.ps1
+++
@@ -1,4 +0,0 @@
-$git = "C:\Program Files\Git\bin\git.exe"
-Set-Location "C:\Users\Usuario\Documents\GitHub\Revisor-IFC"
-& $git push
-Write-Host "PUSH OK"
