3168785a92da1427b01f325efa90b39ecc3e6628
Carlos <carlos@geisterconsultores.com>
2026-03-22 21:59:02 -0300

    fix: vite.config entry point voxelbim.html para build de Cloudflare

--- visor/vite.config.js
+++ visor/vite.config.js
@@ -1,6 +1,11 @@
+import { resolve } from 'path';
+
 export default {
   build: {
     target: 'esnext',
     chunkSizeWarningLimit: 10000,
+    rollupOptions: {
+      input: resolve(__dirname, 'voxelbim.html'),
+    },
   }
 };
